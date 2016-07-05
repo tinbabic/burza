eece8f14adc16cbffe60a1acfe00015e88afab26
Kresimir Sinkovic <kresimirsinko@gmail.com>
2016-07-05 14:49:07 +0200

    dopunjen portfolio

--- view/users_index.php
+++ view/users_index.php
@@ -3,9 +3,18 @@
     <h2>Korisnik <?=$_SESSION['username']?></h2>
     <table>
         <tr>
+            <th> Money </th>
+            <th> Value of stocks</th>
+            <th> Net Worth</th>
+            <th> Overall Gains</th>
+            <th> Overall Returns </th>
+        </tr>
+        <tr>
             <td> <?php echo $money;?></td>
-            <td> <?php echo $stockSum?></td>
-            <td> <?php ?></td>
+            <td> <?php echo $stockSum;?></td>
+            <td> <?php echo $money + $stockSum;?></td>
+            <td> <?php echo $money + $stockSum - 100000.00;?></td>
+            <td> <?php echo ($money + $stockSum - 100000.00)/100000.00*100;?>%</td>
            
         </tr>
     </table>
