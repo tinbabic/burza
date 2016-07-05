fd89145335141d54a6aa69e2f73540db79fe3ad9
Kresimir Sinkovic <kresimirsinko@gmail.com>
2016-07-05 13:04:02 +0200

    stocks i header

--- view/_header.php
+++ view/_header.php
@@ -10,7 +10,7 @@
 	<nav>
 		<ul>
 			<li><a href="<?php echo __SITE_URL; ?>/index.php?rt=users">Portfolio korisnika</a></li>
-			<li><a href="<?php echo __SITE_URL; ?>/index.php?rt=firms">Popis svih firmi</a></li>
+                        <li><a href="<?php echo __SITE_URL; ?>/index.php?rt=stocks">Kupnja i prodaja</a></li>
 			<li><a href="<?php echo __SITE_URL; ?>/index.php?rt=saldos">Saldo korisnika</a></li>
                         <li><a href="<?php echo __SITE_URL; ?>/index.php?rt=transactions">Transakcije</a></li>
                         <li><a href="<?php echo __SITE_URL; ?>/index.php?rt=users/showAllUsers">Top korisnici</a></li>
--- view/stock_history.php
+++ view/stock_history.php
@@ -1,4 +1,10 @@
 <?php
+    require_once '_header.php';?>
+
+<h2> Povijest dionice: <?php echo $name;?></h2>
+        
+<?php
+    echo '<script> var data = [';
     $lastKey = end(array_keys($data));
     foreach($data as $date=>$values){
         echo '{date:"'.$date.'", close:"'.$values[0].'",volume:"'.$values[1].'",dividend:"'.$values[2].'"}';
@@ -6,5 +12,7 @@
             echo ', ';
         }
     }
+    echo ']</script>';
 
+    require_once '_footer.php';
 ?>
